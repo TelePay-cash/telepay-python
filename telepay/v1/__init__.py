c24ce620b333ac63784a8d06b28ec7e547d01c93
Ravenclawldz <ravenclaw.ldz@gmail.com>
2022-04-01 19:10:26 -0400

    updating source code

--- telepay/v1/__init__.py
+++ telepay/v1/__init__.py
@@ -1,1 +1,9 @@
-__version__ = '0.0.1'
+from ._sync.client import SyncTelePayClient
+from .auth import TelePayAuth
+from .errors import TelePayError
+from .models.info import Info
+from .models.assets import Assets
+from .models.invoice import Invoice
+from .models.balance import Balance
+from .models.transfer import Transfer
+from .models.withdraw import Withdraw
