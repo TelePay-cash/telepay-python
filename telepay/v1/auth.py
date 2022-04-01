b8b152e2d8838e8d2842d66032c60182b93b48da
Ravenclawldz <ravenclaw.ldz@gmail.com>
2022-04-01 19:10:26 -0400

    updating source code

--- telepay/v1/auth.py
+++ telepay/v1/auth.py
@@ -1,4 +1,3 @@
-from os import environ
 from dotenv import load_dotenv
 from dataclasses import dataclass, field
 
@@ -10,12 +9,12 @@ load_dotenv()
 
 @dataclass
 class TelePayAuth:
-    api_public: str = field(default=environ["API_PUBLIC"])
-    api_secret: str = field(default=environ["API_SECRET"])
+    api_public: str
+    api_secret: str
 
     def __post_init__(self):
         if not self.api_public or not self.api_secret:
-            raise TelePayError("API_PUBLIC and API_SECRET must be set in .env")
+            raise TelePayError(0, "API_PUBLIC and API_SECRET must be set in .env")
         if not self.api_public:
             raise TelePayError(0, "API_PUBLIC is not setted")
         if not self.api_secret:
