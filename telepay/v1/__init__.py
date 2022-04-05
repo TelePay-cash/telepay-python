37ce49ea4062a31c22a62bf15a2a552147ec9766
Carlos Lugones <contact@lugodev.com>
2022-04-05 19:02:23 -0400

    chore: ready for v.0.0.2

--- telepay/v1/__init__.py
+++ telepay/v1/__init__.py
@@ -5,7 +5,6 @@ from .errors import TelePayError  # noqa: F401
 from .models.account import Account  # noqa: F401
 from .models.assets import Assets  # noqa: F401
 from .models.invoice import Invoice  # noqa: F401
-from .models.transfer import Transfer  # noqa: F401
 from .models.wallets import Wallets  # noqa: F401
 from .models.withdraw import Withdraw  # noqa: F401
 from .webhooks import TelePayWebhookListener  # noqa: F401
--- telepay/v1/webhooks.py
+++ telepay/v1/webhooks.py
@@ -9,10 +9,10 @@ from fastapi import FastAPI, Request
 from .errors import TelePayError
 
 # events
-INVOICE_COMPLETED = 'invoice.completed'
-INVOICE_CANCELLED = 'invoice.cancelled'
-INVOICE_EXPIRED = 'invoice.expired'
-INVOICE_DELETED = 'invoice.deleted'
+INVOICE_COMPLETED = "invoice.completed"
+INVOICE_CANCELLED = "invoice.cancelled"
+INVOICE_EXPIRED = "invoice.expired"
+INVOICE_DELETED = "invoice.deleted"
 
 
 def get_signature(data, secret):
