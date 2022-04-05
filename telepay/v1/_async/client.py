522f4f36fe059fc014cff47dbf913ab0937dde61
Carlos Lugones <contact@lugodev.com>
2022-04-05 19:09:30 -0400

    perf: clients

--- telepay/v1/_async/client.py
+++ telepay/v1/_async/client.py
@@ -9,6 +9,7 @@ from ..models.account import Account
 from ..models.assets import Assets
 from ..models.invoice import Invoice, InvoiceList
 from ..models.wallets import Wallets
+from ..models.withdraw import Withdraw
 from ..utils import validate_response
 
 
@@ -133,3 +134,85 @@ class TelePayAsyncClient:
         response = await self.http_client.post(f"deleteInvoice/{number}")
         validate_response(response)
         return response.json()
+
+    async def transfer(
+        self,
+        asset: str,
+        blockchain: str,
+        network: str,
+        amount: float,
+        username: str,
+        message: str,
+    ) -> dict:
+        """
+        Transfer funds between internal wallets.
+        Off-chain operation.
+        """
+        response = await self.http_client.post(
+            "transfer",
+            json={
+                "asset": asset,
+                "blockchain": blockchain,
+                "network": network,
+                "amount": amount,
+                "username": username,
+                "message": message,
+            },
+        )
+        validate_response(response)
+        return response.json()
+
+    async def withdraw(
+        self,
+        asset: str,
+        blockchain: str,
+        network: str,
+        amount: float,
+        to_address: str,
+        message: str,
+    ) -> Withdraw:
+        """
+        Withdraw funds from merchant wallet to external wallet.
+        On-chain operation.
+        """
+        # response = await self.http_client.post(
+        #     "withdraw",
+        #     json={
+        #         "asset": asset,
+        #         "blockchain": blockchain,
+        #         "network": network,
+        #         "amount": amount,
+        #         "to_address": to_address,
+        #         "message": message,
+        #     },
+        # )
+        # validate_response(response)
+        # return Withdraw.from_json(response.json())
+        raise NotImplementedError()
+
+    async def getWithdrawFee(
+        self,
+        asset: str,
+        blockchain: str,
+        network: str,
+        amount: float,
+        to_address: str,
+        message: str,
+    ) -> Withdraw:
+        """
+        Get estimated withdraw fee, composed of blockchain fee and processing fee.
+        """
+        # response = await self.http_client.post(
+        #     "getWithdrawFee",
+        #     json={
+        #         "asset": asset,
+        #         "blockchain": blockchain,
+        #         "network": network,
+        #         "amount": amount,
+        #         "to_address": to_address,
+        #         "message": message,
+        #     },
+        # )
+        # validate_response(response)
+        # return Withdraw.from_json(response.json())
+        raise NotImplementedError()
