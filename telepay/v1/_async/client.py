40bf71bd885883e24857138d9783ac7cf31ebf10
Carlos Lugones <contact@lugodev.com>
2022-04-26 17:35:57 -0400

    fix: positional arguments missing on sync client __exit__ method #14
    closes #14

--- telepay/v1/_async/client.py
+++ telepay/v1/_async/client.py
@@ -34,7 +34,7 @@ class TelePayAsyncClient:
     async def __aenter__(self) -> "TelePayAsyncClient":
         return self
 
-    async def __aexit__(self, exc_t, exc_v, exc_tb) -> None:
+    async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
         await self.close()
 
     async def close(self) -> None:
