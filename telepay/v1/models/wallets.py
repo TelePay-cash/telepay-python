7f588e58d5997d2114c22de172e51accc6b3c03d
Carlos Lugones <contact@carloslugones.com>
2022-06-08 16:05:45 -0400

    feat: POST /getBalance endpoint

--- telepay/v1/models/wallets.py
+++ telepay/v1/models/wallets.py
@@ -5,8 +5,20 @@ from ..utils import parse_json
 
 
 @dataclass
+class Wallet:
+    asset: str
+    blockchain: str
+    network: str
+    balance: float
+
+    @classmethod
+    def from_json(cls, json: Any) -> "Wallet":
+        return parse_json(cls, **json)
+
+
+@dataclass
 class Wallets:
-    wallets: list
+    wallets: list[Wallet]
 
     def __post_init__(self):
         pass
